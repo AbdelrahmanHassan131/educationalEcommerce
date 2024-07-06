2800df99244380bba954c0f5949e5b06a781e3ab
Abdelrahman <abdelrahmanhassanismail131@gmail.com>
2024-07-06 14:49:44 +0300

    Setup Entity framework

---
+++ educationalEcommerce.Server/Infrastructure/EduCommDbContext.cs
@@ -0,0 +1,25 @@
+﻿using educationalEcommerce.Server.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace educationalEcommerce.Server.Infrastructure
+{
+    public class EduCommDbContext : DbContext
+    {
+        public IConfiguration _configuration { get; set; }
+
+        public EduCommDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DatabaseConnection"));
+
+        }
+
+        public DbSet<TryEntity> TryEntities { get; set; }
+    }
+
+}
