c519b7e2bb7214ce2004d2409168cc33781fb694
Abdelrahman <abdelrahmanhassanismail131@gmail.com>
2025-01-18 23:20:53 +0200

    add react router dom and two temproray pages

---
+++ educationalecommerce.client/src/Pages/Home/Home.tsx
@@ -0,0 +1,19 @@
+import { Stack, Typography } from "@mui/material";
+
+export function HomePage() {
+  return (
+    <Stack sx={{ background: "red" }} width={"100%"}>
+      <Typography>
+        Hello Form Home pageHello Form Home pageHello Form Home pageHello Form
+        Home pageHello Form Home pageHello Form Home pageHello Form Home
+        pageHello Form Home pageHello Form Home page
+      </Typography>
+      <Typography>Hello Form Home page</Typography>
+      <Typography>Hello Form Home page</Typography>
+      <Typography>Hello Form Home page</Typography>
+      <Typography>Hello Form Home page</Typography>
+      <Typography>Hello Form Home page</Typography>
+      <Typography>Hello Form Home page</Typography>
+    </Stack>
+  );
+}
